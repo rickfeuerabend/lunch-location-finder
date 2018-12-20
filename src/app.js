0c7323901df92190a61266676c2c21f4dd8f83f4
Rick Feuerabend <rfeuerabend@team.mobile.de>
2018-12-20 15:53:49 +0100

    updae

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import sample from "lodash/sample"
+import locations from "./config/locations"
+import { getWeekDay, getWeekDayOfTomorrow } from "./uitls"
+
+export default function getLocation() {
+  return sample(locations)
+}
