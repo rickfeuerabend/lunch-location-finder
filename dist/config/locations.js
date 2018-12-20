2ca4996b3e919f4041b46cfb6b7db6531919b771
Rick Feuerabend <rfeuerabend@team.mobile.de>
2018-12-20 16:54:46 +0100

    updae

--- dist/config/locations.js
+++ dist/config/locations.js
@@ -3,10 +3,38 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _nodeFetch = require("node-fetch");
+
+var _nodeFetch2 = _interopRequireDefault(_nodeFetch);
+
+var _cheerio = require("cheerio");
+
+var _cheerio2 = _interopRequireDefault(_cheerio);
+
+var _get = require("lodash/get");
+
+var _get2 = _interopRequireDefault(_get);
+
+var _uitls = require("../uitls");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 exports.default = [{
   name: "Liebstöckel",
   info: function info() {
-    return "yeahhh";
+    return (0, _nodeFetch2.default)("https://liebstoeckel-tagesbar.de/mittagskarte/").then(function (responseRandom) {
+      return responseRandom.text();
+    }).then(function (res) {
+      var $ = _cheerio2.default.load(res);
+
+      var menu = $("#main").find(".entry-content").text();
+
+      var re = /Donnerstag(.*)Freitag/gms;
+      var re = new RegExp((0, _uitls.getWeekDay)() + "(.*)" + (0, _uitls.getWeekDayOfTomorrow)(), "gms");
+
+      return re.exec(menu)[0];
+    });
   }
 }, {
   name: "Azad Grill Jena"
@@ -27,7 +55,15 @@ exports.default = [{
 }, {
   name: "Sushi Ninjas"
 }, {
-  name: "Mekkan Döner"
+  name: "Mekkan Döner",
+  info: function info() {
+    var menu = {
+      Montag: "Pizza Tag",
+      Dienstag: "Dönertag",
+      Freitag: "Nudeltag"
+    };
+    return (0, _get2.default)(menu, _uitls.getWeekDay, "Normales Menü");
+  }
 }, {
   name: "Syrisches Restraurant"
 }, {
@@ -41,13 +77,15 @@ exports.default = [{
 }, {
   name: "Rosenmensa"
 }, {
-  name: "Bunshi"
+  name: "Bunshi",
+  info: "Lust auf Spaziergang?"
 }, {
   name: "Immergrün"
 }, {
   name: "Grünovski"
 }, {
-  name: "Teufelsgrill 😈 "
-}, {
-  name: "Immergrün"
+  name: "Teufelsgrill 😈 ",
+  info: function info() {
+    return "Bratwurst & Rostbrätel ohne Ende";
+  }
 }];
--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,6 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+require("babel-register")({
+  presets: ["env"]
 });
-exports.default = getLocation;
-
-var _sample = require("lodash/sample");
-
-var _sample2 = _interopRequireDefault(_sample);
-
-var _locations = require("./config/locations");
-
-var _locations2 = _interopRequireDefault(_locations);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function getLocation(name) {
-  return (0, _sample2.default)(_locations2.default);
-}
+module.exports = require("./app.js");
